21fa64656fe52a4bdcd182452bf71b1f361b0174
shravanij22 <147096064+shravanij22@users.noreply.github.com>
2025-01-23 20:58:55 +0530

    ****

--- app/src/main/java/com/example/nhitmclassboard/navigation/NavGraph.kt
+++ app/src/main/java/com/example/nhitmclassboard/navigation/NavGraph.kt
@@ -3,6 +3,11 @@ package com.example.nhitmclassboard.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import com.example.nhitmclassboard.screens.AboutUs
+import com.example.nhitmclassboard.screens.BottomNav
+import com.example.nhitmclassboard.screens.Faculty
+import com.example.nhitmclassboard.screens.Gallery
+import com.example.nhitmclassboard.screens.Home
 
 
 @Composable
@@ -11,8 +16,33 @@ fun NavGraph(navController: NavHostController){
         navController = navController,
         startDestination = Routes.BottomNav.route
     ){
-        composable()
+         composable(Routes.BottomNav.route){
+            BottomNav(navController)
+        }
+
+        composable(Routes.BottomNav.route){
+            Home()
+        }
+
+        composable(Routes.BottomNav.route){
+            Gallery()
+        }
+
+        composable(Routes.BottomNav.route){
+            Faculty()
+        }
+
+        composable(Routes.BottomNav.route){
+            AboutUs()
+        }
 
     }
 
 }
+
+
+fun composable(route: String, function: @Composable () -> Unit) {
+
+}
+
+
